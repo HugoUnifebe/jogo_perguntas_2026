fcbddec960399589c67e2aa0b8e6d6fdea3ad8ae
Hugo Sens <victor.sens@unifebe.edu.br>
2026-05-13 22:08:43 -0300

    exemplo JSON, ajustes de layout e parte 3

--- assets/js/parte1.js
+++ assets/js/parte1.js
@@ -1,16 +1,18 @@
 /** PARTE 1**/
-let btn = $(`.parte-1 .botao-iniciar`);
-btn.on(`click`, e => {
-    trocaParte(2);
-});
-btn.on("mousedown", function () {
-    btn.addClass(`clicado`);
-    playSound('click.mp3', .5);
-});
+(e => {
+    let btn = $(`.parte-1 .botao-iniciar`);
+    btn.on(`click`, e => {
+        trocaParte(2);
+    });
+    btn.on("mousedown", function () {
+        btn.addClass(`clicado`);
+        playSound('click.mp3', .5);
+    });
 
-btn.on("mouseup mouseleave", function () {
-    btn.removeClass(`clicado`);
-});
-btn.get(0).addEventListener('mouseover', () => {
-    playSound('hover.mp3', .05);
-});
+    btn.on("mouseup mouseleave", function () {
+        btn.removeClass(`clicado`);
+    });
+    btn.get(0).addEventListener('mouseover', () => {
+        playSound('hover.mp3', .05);
+    });
+})();
