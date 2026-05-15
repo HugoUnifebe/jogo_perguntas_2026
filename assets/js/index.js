141f8ba0419ccb1b317f9d874eea6920c56a1a4c
Hugo Sens <victor.sens@unifebe.edu.br>
2026-05-14 21:49:27 -0300

    programação do jogo

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,9 +5,40 @@ let trocaParte = (n) => {
 
 function playSound(soundFileName, nivelSom = 1) {
     const audio = new Audio(`./assets/sound/${soundFileName}`);
-    if(soundFileName === 'click.mp3'){
+    if (soundFileName === 'click.mp3') {
         audio.currentTime = .5;
     }
     audio.volume = nivelSom
     audio.play().catch(e => console.error("Erro ao tocar o som:", e));
+}
+
+
+function montarHtmlColorido(frase, tag = 'h4') {
+    const $h1 = $(`<${tag}>`, {
+        class: 'colorido'
+    });
+    const palavras = frase.split(' ');
+    palavras.forEach((palavra, index) => {
+        const $wrapper = $('<span>', {
+            class: 'text-nowrap'
+        });
+        if (index > 0) {
+            $wrapper.append('&nbsp;');
+        }
+        palavra.split('').forEach(letra => {
+            $wrapper.append(
+                $('<span>').text(letra)
+            );
+        });
+        $h1.append($wrapper);
+    });
+    return $h1;
+}
+
+function embaralharArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
